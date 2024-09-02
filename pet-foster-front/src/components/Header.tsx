dcf7224155641b17cc4912ce628e30535e172eaf
YOUR_USERNAME <navarro.romain@gmail.com>
2024-09-02 10:56:30 +0200

    improved header

--- pet-foster-front/src/components/Header.tsx
+++ pet-foster-front/src/components/Header.tsx
@@ -23,25 +23,28 @@ function Header() {
             <img src="/img/vector/petfoster-logo-grad.svg" alt="Logo" width={100}/>
             <img src="/img/vector/petfoster-title-grad.svg" alt="Pet Foster" width={200}/>
           </Navbar.Item>
-          
-        </Navbar.Brand>
-        <Navbar.Burger
+          <Navbar.Burger
             className={isActive ? 'is-active' : ''}
             onClick={handleBurgerClick}
           />
+        </Navbar.Brand>
+        
 
         <Navbar.Menu className={isActive ? 'is-active' : ''}>
           <Navbar.Container align="right">
-            <Navbar.Item href="/trouver-animal">Voir les animaux</Navbar.Item>
-            <Navbar.Item href="/mes-demandes">Mes demandes</Navbar.Item>
-            <Navbar.Item href="/mes-animaux">Mes animaux</Navbar.Item>
-            {/* <Navbar.Item dropdown hoverable>
-              <Navbar.Link>More</Navbar.Link>
-              <Navbar.Dropdown>
-                <Navbar.Item href="/connexion">Connexion</Navbar.Item>
-                <Navbar.Item href="/mon-compte">Mon compte</Navbar.Item>
-              </Navbar.Dropdown>
-            </Navbar.Item> */}
+            {/* <Navbar.Item href="/trouver-animal" renderAs='NavLink'>Voir les animaux</Navbar.Item> */}
+            <NavLink to="/trouver-animal" exact className="navbar-item">
+              Voir les animaux
+            </NavLink>
+            <NavLink to="/mes-demandes" exact className="navbar-item">
+              Mes demandes
+            </NavLink>
+            <NavLink to="/mes-animaux" exact className="navbar-item">
+              Mes animaux
+            </NavLink>
+            {/* <Navbar.Item href="/mes-demandes">Mes demandes</Navbar.Item> */}
+            {/* <Navbar.Item href="/mes-animaux">Mes animaux</Navbar.Item> */}
+
             <Navbar.Item href="/connexion">< User size={24} /></Navbar.Item>
           </Navbar.Container>
         </Navbar.Menu>
